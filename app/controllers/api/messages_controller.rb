7931e1fe80cc4117e6d201abfb313c5a3631178c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-23 16:26:10 +0630

    Update linters.yml

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
-    def random_greeting
-        message = Message.order('RANDOM()').first
-        render json: {gretting: message.content}
-    end
+  def random_greeting
+    message = Message.order('RANDOM()').first
+    render json: { gretting: message.content }
+  end
 end
