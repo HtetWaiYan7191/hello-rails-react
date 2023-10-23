7d16e534593d472bcfeec70988ea6f4dda6dc1ff
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-23 16:24:57 +0630

    Set up linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'root#index'
 
   namespace :api do
     get 'random_greeting', to: 'messages#random_greeting'
